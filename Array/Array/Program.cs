d672ba655b7f4e96aa50014218cecf1ec0baa2d2
metin.sen <metin.sen@ALS-YG39506-J06>
2016-11-19 18:20:41 +0300

    1

---
+++ Array/Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+        }
+    }
+}
