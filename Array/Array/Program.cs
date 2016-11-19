53fa0a23984f2ad0153e48a6184472967d39a10a
metin.sen <metin.sen@ALS-YG39506-J06>
2016-11-19 18:46:48 +0300

    2

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -10,17 +10,34 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            int[] array = new int[3];
-            for (int i = 0; i < array.Length; i++)
+            //int[] dizi = new int[3];
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //    dizi[i] = i;
+            //}
+
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+
+            //    Console.WriteLine(dizi[i]);
+
+            //}
+            //Console.ReadKey();
+
+            Console.Write("ınıf Mevcıdunu Girin : ");
+            int mevcut = Convert.ToInt32(Console.ReadLine());
+            string[] ogrenciler = new string[mevcut];
+
+            for (int i = 0; i < ogrenciler.Length; i++)
             {
-                array[i] = i;
+                Console.Write("{0}.Öğrenci ismini giriniz:",i);
+                ogrenciler[i] = Console.ReadLine();
             }
-
-            for (int i = 0; i < array.Length; i++)
+            Console.Clear();
+            Console.WriteLine("Ögrenci Listesi\n");
+            foreach(var item in ogrenciler)
             {
-
-                Console.WriteLine(array[i]);
-                
+                Console.WriteLine(item);
             }
             Console.ReadKey();
         }
